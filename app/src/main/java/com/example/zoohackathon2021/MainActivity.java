49206002afd2625a6d13b9c0c5178f32c76a2cda
hoangesoftblog <hoangviethoa123@yahoo.com.vn>
2021-11-15 11:18:36 +0700

    First commit

---
+++ app/src/main/java/com/example/zoohackathon2021/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.zoohackathon2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+
+}
